0fff1bf55eb9e5a7a2b3ff309b4604d4e3633558
ShyTech1 <shaikelman@gmail.com>
2022-07-19 14:28:25 +0300

    right bar skeleton

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -3,7 +3,13 @@ import "./rightbar.css";
 export default function rightBar() {
   return (
     <div className="rightBarContainer">
-
+      <div className="rightBarUpdates"> updates</div>
+      <div className="rightBatAd">
+        <img src="" alt="" className="rightBarAdImg" />
+      </div>
+      <div className="rightBarOnlineFriends">
+        
+      </div>
     </div>
   )
 }
