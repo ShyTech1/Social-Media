f45734cd5a6a21a6e32728f13f0a958121759f70
ShyTech1 <shaikelman@gmail.com>
2022-07-20 11:55:47 +0300

    right bar

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -3,12 +3,19 @@ import "./rightbar.css";
 export default function rightBar() {
   return (
     <div className="rightBarContainer">
-      <div className="rightBarUpdates"> updates</div>
+      <div className="rightBarUpdates"> 
+        <img src="./assets/gift.png" alt="" className="rightBarUpdatesImg" />
+        <p className="rightBarUpdatesText"><b>Tedi Bear</b> and <b>2 other friends </b>have a birthday today </p>      
+      </div>
       <div className="rightBatAd">
-        <img src="" alt="" className="rightBarAdImg" />
+        <img src="./assets/ad.jfif" alt="AD" className="rightBarAdImg" />
       </div>
       <div className="rightBarOnlineFriends">
-        
+        <span className="rightBaronlineFreindsTitle">Online Friends</span>
+        <div className="rightBarOnlineFriendsList">
+          <div className="rightBarOnlineFriendsImgWrapper"><img src="../assets/person/7.jpg" alt="" className="rightBarOnlineFriendsImg" /></div>
+          <span className="rightBarOnlineFriendsName">Jonny Walker</span>
+        </div>
       </div>
     </div>
   )
